1b5e79d34e3604dc86bebaccf52dd10a3d3bc34e
Pervez Iqbal <pervefunctor@gmail.com>
2024-12-26 05:02:32 +0530

    initial commit

---
+++ .config/sway/env.sh
@@ -0,0 +1,18 @@
+export XCURSOR_SIZE=32
+export CLUTTER_BACKEND=wayland
+export GDK_BACKEND=wayland,x11
+export QT_AUTO_SCREEN_SCALE_FACTOR=1.5
+export # QT_QPA_PLATFORM=wayland;xcb
+export #=QT_QPA_PLATFORMTHEME,qt5ct
+export QT_QPA_PLATFORMTHEME=qt6ct
+export QT_SCALE_FACTOR=1.5
+export #=QT_WAYLAND_DISABLE_WINDOWDECORATION,1
+export XDG_SESSION_TYPE=wayland
+
+export #=GDK_SCALE,1
+export MOZ_ENABLE_WAYLAND=1
+
+# electron >28 apps (may help)`
+export ELECTRON_OZONE_PLATFORM_HINT=auto
+export OZONE_PLATFORM=wayland
+export ELECTRON_OZONE_PLATFORM_HINT=wayland
